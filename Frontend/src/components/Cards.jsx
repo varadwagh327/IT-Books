d5fcda6e239cbe72d5706fda56b81dd76adca95d
Varad Wagh <varadwagh327@gmail.com>
2025-05-06 16:39:41 +0530

    some changes I commit

--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -18,7 +18,7 @@ function Cards({ item }) {
             <div className="card-actions justify-between">
               <div className="badge badge-outline">${item.price}</div>
               <div className=" cursor-pointer px-2 py-1 rounded-full border-[2px] hover:bg-pink-500 hover:text-white duration-200">
-                Buy Now
+                <a href="/about">Buy Now</a>
               </div>
             </div>
           </div>
